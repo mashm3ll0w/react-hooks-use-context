f80f1fc088049749b269c9ec49d1cf6ec81119f8
Charles Swaleh <charlswall@live.com>
2023-02-10 11:37:56 +0300

    Completed creating the user context

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import Header from "./Header";
 import Profile from "./Profile";
+import { UserProvider } from "../context/user";
 
 function App() {
-  const [theme, setTheme] = useState("dark");
-  const [user, setUser] = useState(null);
-  return (
-    <main className={theme}>
-      <Header theme={theme} setTheme={setTheme} user={user} setUser={setUser} />
-      <Profile theme={theme} user={user} />
-    </main>
-  );
+	const [theme, setTheme] = useState("dark");
+	return (
+		<main className={theme}>
+			<UserProvider>
+				<Header theme={theme} setTheme={setTheme} />
+				<Profile theme={theme} />
+			</UserProvider>
+		</main>
+	);
 }
 
 export default App;
